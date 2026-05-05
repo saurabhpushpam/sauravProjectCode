ba3825625b5c31c7be4846b0e1ca6d3ab6fee2dc
saurabhpushpam <145956198+saurabhpushpam@users.noreply.github.com>
2026-05-05 15:18:59 +0530

    add code for removeduplicate

--- Array/removeDuplicate.js
+++ Array/removeDuplicate.js
@@ -79,3 +79,12 @@ console.log(removeDuplicates([]));
 
 // practice :
 
+
+ let arr= [1, 2, 3, 4, 5, 3, 2];
+ let newarr =[];
+ for(let i= 0; i< arr.length; i++){
+  if (!newarr.includes(arr[i])){
+   newarr.push(arr[i]);
+ }
+ }
+ console.log(newarr);
